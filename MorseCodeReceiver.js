c1e2c72c0a194f652cf0b994166acd8bc5ff8433
QZulkifli <nazul5@student.monash.edu>
2016-04-22 10:56:37 +1000

    new morse code

--- MorseCodeReceiver.js
+++ MorseCodeReceiver.js
@@ -1,45 +1,27 @@
-/*
- * Morse Code receiver app information:
- *
- * Function: messageFinished(): stops the capturing process
- *
- *     You can call this function to let the app know that the 
- *     end-of-transmission signal has been received.
- *
- * -------------------------------------------------------
- *
- * ID: messageField: id of the message text area
- *
- *     This will be a textarea element where you can display
- *     the recieved message for the user.
- * 
- * -------------------------------------------------------
- *
- * ID: restartButton: id of the Restart button
- *
- *     This is a button element.  When clicked this should 
- *     cause your app to reset its state and begin recieving
- *     a new message.
- *
- */
 
-
-// ADD YOUR ADDITIONAL FUNCTIONS AND GLOBAL VARIABLES HERE
-
-
-/*
- * This function is called once per unit of time with camera image data.
- * 
- * Input : Image Data. An array of integers representing a sequence of pixels.
- *         Each pixel is representing by four consecutive integer values for 
- *         the 'red', 'green', 'blue' and 'alpha' values.  See the assignment
- *         instructions for more details.
- * Output: You should return a boolean denoting whether or not the image is 
- *         an 'on' (red) signal.
- */
 function decodeCameraImage(data)
 {
     // ADD YOUR CODE HERE
 
     return false;
+}
+function convertToCharacter (morseString)
+{
+	//check if the message has already finished (check whether the morse code for SK exist at the end of sentence)
+	
+	if (morseString != '...-.-')
+	{
+		// will continue to display message because there is no SK morse code detected
+		
+		var char = morseToCharacterTable[morseString]
+		displayResult.innerHTML += char
+	}
+	
+	if (morseString == '...-.-')
+	{	
+		// function message finished is called to indicate the message has already finished
+        
+		messageFinished()
+	}
+	
 }
